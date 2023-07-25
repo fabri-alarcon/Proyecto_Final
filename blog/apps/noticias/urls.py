f3f6ac914bededc18f26889c94fe4c836876c68f
Fabricio <alarconfabri800@gmail.com>
2023-07-25 11:23:41 -0300

    first commit

---
+++ blog/apps/noticias/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+app_name = "noticias"
+
+# urls de app noticias
+urlpatterns = [
+    path("", views.inicio, name= "inicio"),
+     # url para el detalle de la noticia por pk
+    path('detalle<int:pk>', views.Detalle_Noticias, name='detalle'),
+
+    # url del formulario de contacto
+    path('contacto', views.contacto, name="contacto"),
+
+      # URL COMENTARIO
+    path('comentario', views.Comentar_Noticia, name='comentar')
+    
+
+]
